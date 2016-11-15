ffd9c7096bb6155337a155b9dd89c4765c1524a2
hungnh103 <hungnh103@gmail.com>
2016-11-15 10:37:00 +0700

    [Salesmanager] Mini navigation

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Herbal Tea</title>
+    <title>Quỳnh Phương Herbal Tea</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
     <link rel="stylesheet" type="text/css" href="assets/stylesheets/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="assets/stylesheets/custom.css" />
