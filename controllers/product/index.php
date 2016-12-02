b47d1733c3a90bc4e434c69c8c69e691c8e79846
hungnh103 <hungnh103@gmail.com>
2016-12-02 11:53:53 +0700

    [User][List Products] Paginate

--- controllers/product/index.php
+++ controllers/product/index.php
@@ -1,8 +1,25 @@
 <?php
-$mproduct = new Model_Product;
-$mproduct->order("pid", "DESC");
-$data['product'] = $mproduct->listProduct();
 $meffect = new Model_Effect;
 $meffect->order("oeid", "DESC");
 $data['effect'] = $meffect->listEffect();
+
+$per_page = 12;
+$mproduct = new Model_Product;
+if(isset($_GET['oeid']) && $_GET['oeid'] != 0) {
+  $mproduct->where("oeid = '$_GET[oeid]'");
+}
+$mproduct->listProduct();
+$data['items_amount'] = $mproduct->num_rows();
+$data['page'] = ceil($data['items_amount'] / $per_page);
+$mproduct->order("pid", "DESC");
+if(isset($_GET['page'])) {
+  $start = 12 * ($_GET['page'] - 1);
+  $mproduct->limit($per_page, $start);
+  $data['current_page'] = $_GET['page'];
+} else {
+  $mproduct->limit($per_page);
+  $data['current_page'] = 1;
+}
+$data['product'] = $mproduct->listProduct();
+
 loadview("product/index", $data);
--- views/product/index.php
+++ views/product/index.php
@@ -1,15 +1,34 @@
 <?php
 loadview("layouts/header");
 ?>
+<script type="text/javascript">
+  $(document).ready(function(){
+    $("#user_product_filter").change(function(){
+      oeid = $("#user_product_filter").val();
+      $("#products_filter form").attr("action", "index.php?controller=product&page=1&oeid="+oeid+"");
+      $("#products_filter form").submit();
+    });
+  });
+</script>
+
 <div id="list_products_page" style="min-height: 252px;">
   <div id="products_filter">
-    <form>
+    <form method="post">
       <span>Hiển thị sản phẩm theo công dụng nổi bật</span>
-      <select class="form-control" style="width: 200px; float: left;">
+      <select class="form-control" style="width: 350px; float: left;" id="user_product_filter">
         <option value='0'>-- Tất cả --</option>
         <?php
+        if (isset($_GET['oeid'])) {
+          $choosen_oeid = $_GET['oeid'];
+        } else {
+          $choosen_oeid = 0;
+        }
         foreach($data['effect'] as $item){
-          echo "<option value='$item[oeid]'>$item[content]</option>";
+          if ($item['oeid'] == $choosen_oeid) {
+            echo "<option value='$item[oeid]' selected='selected'>$item[content]</option>";
+          } else {
+            echo "<option value='$item[oeid]'>$item[content]</option>";
+          }
         }
         ?>
       </select>
@@ -33,6 +52,60 @@ loadview("layouts/header");
     }
     ?>
   </div>
+
+  <div class="clr"></div>
+
+  <nav aria-label="Page navigation" style="position: relative; height: 77px;">
+    <ul class="pagination" style="position: absolute; right: 0px; bottom: 0px;">
+      <?php
+      if ($data['current_page'] == 1) {
+        echo "<li class='disabled'>";
+            echo "<span aria-hidden='true'>&laquo;</span>";
+        echo "</li>";
+      } else {
+        $prev = $data['current_page'] - 1;
+        echo "<li>";
+          if(isset($_GET['oeid'])) {
+            echo "<a href='index.php?controller=product&page=$prev&oeid=$choosen_oeid' aria-label='Previous'>";
+          } else {
+            echo "<a href='index.php?controller=product&page=$prev' aria-label='Previous'>";
+          }
+            echo "<span aria-hidden='true'>&laquo;</span>";
+          echo "</a>";
+        echo "</li>";
+      }
+
+      for ($i = 1; $i <= $data['page']; $i++) {
+        if ($i == $data['current_page']) {
+          echo "<li class='active'><a href='javascript: void(0)'>$i</a></li>";
+        } else {
+          if(isset($_GET['oeid'])) {
+            echo "<li><a href='index.php?controller=product&page=$i&oeid=$choosen_oeid'>$i</a></li>";
+          } else {
+            echo "<li><a href='index.php?controller=product&page=$i'>$i</a></li>";
+          }
+        }
+      }
+
+      if ($data['current_page'] == $data['page']) {
+        echo "<li class='disabled'>";
+            echo "<span aria-hidden='true'>&raquo;</span>";
+        echo "</li>";
+      } else {
+        $next = $data['current_page'] + 1;
+        echo "<li>";
+          if(isset($_GET['oeid'])) {
+            echo "<a href='index.php?controller=product&page=$next&oeid=$choosen_oeid' aria-label='Next'>";
+          } else {
+            echo "<a href='index.php?controller=product&page=$next' aria-label='Next'>";
+          }
+            echo "<span aria-hidden='true'>&raquo;</span>";
+          echo "</a>";
+        echo "</li>";
+      }
+      ?>
+    </ul>
+  </nav>
 </div>
 <?php
 loadview("layouts/footer");
